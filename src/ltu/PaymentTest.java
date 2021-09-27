1d92d600339cebebb5b7602eaea4494adf57cc08
JohanOlsson99 <51018314+JohanOlsson99@users.noreply.github.com>
2021-09-27 11:04:25 +0200

    change system

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -4,12 +4,19 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
 
 import org.junit.After;
 import org.junit.Test;
 
-public class PaymentTest {
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CalendarImpl.class})
+public class PaymentTest<RunWith> {
     private CalendarImpl cal = new CalendarImpl();
 
     
@@ -85,50 +92,71 @@ public class PaymentTest {
         assertEquals(9904, payment.getMonthlyAmount("20000101-0000", 0, 100, 51));    // completion ratio >
     }
     
-    @Test @After
+    @Test
     public void testError() throws IOException {
         PaymentImpl payment = new PaymentImpl(this.cal);
         try {
             payment.getMonthlyAmount(null, 0, 100, 100);    // error thrown
-            fail("Expected exception did not occur");
+            fail("Expected exception did not occur");   // if no error thrown, cast a fail
         } catch (IllegalArgumentException e) {
             assertEquals("Invalid input.", e.getMessage());
         }
 
         try {
             payment.getMonthlyAmount("20000101-0000", -1, 100, 100);    // error thrown
-            fail("Expected exception did not occur");
+            fail("Expected exception did not occur");   // if no error thrown, cast a fail
         } catch (IllegalArgumentException e) {
             assertEquals("Invalid input.", e.getMessage());
         }
 
         try {
             payment.getMonthlyAmount("20000101-0000", 0, -1, 100);    // error thrown
-            fail("Expected exception did not occur");
+            fail("Expected exception did not occur");   // if no error thrown, cast a fail
         } catch (IllegalArgumentException e) {
             assertEquals("Invalid input.", e.getMessage());
         }
 
         try {
             payment.getMonthlyAmount("20000101-0000", 0, 100, -1);    // error thrown
-            fail("Expected exception did not occur");
+            fail("Expected exception did not occur");   // if no error thrown, cast a fail
         } catch (IllegalArgumentException e) {
             assertEquals("Invalid input.", e.getMessage());
         }
         
         try{
             payment.getMonthlyAmount("200001010000", 0, 100, 0);    // error thrown
-            fail("Expected exception did not occur");
+            fail("Expected exception did not occur");   // if no error thrown, cast a fail
         } catch (IllegalArgumentException e) {
             assertEquals("Invalid personId: 200001010000", e.getMessage());
         }
         
         try{
             payment.getMonthlyAmount("20000101-00000", 0, 100, 0);    // error thrown
-            fail("Expected exception did not occur");
+            fail("Expected exception did not occur");   // if no error thrown, cast a fail
         } catch (IllegalArgumentException e) {
             assertEquals("Invalid personId: 20000101-00000", e.getMessage());
         }
+    }
+
+    @Test
+    public void testErrorConstructor() throws IOException {
+        try {
+            new PaymentImpl(this.cal, null);
+            fail("Expected exception did not occur");   // if no error thrown, cast a fail
+        } catch (Exception e) { }
+    }
+
+    @Test @After
+    public void testPaymentDay() throws IOException {
+        System.out.println("cal " + this.cal.getDate());
+        mockStatic(Calendar.class);
+        when(Calendar.getInstance()).thenReturn(calendar);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, 1); // day
+        calendar.set(Calendar.MONTH, 1); // month
+        calendar.set(Calendar.YEAR, 1999); // year        
+        PaymentImpl payment = new PaymentImpl(this.cal);
+        System.out.println(payment.getNextPaymentDay());
     }
 
 }
